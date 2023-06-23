fc4325142a853c0b36e649fe054bcbd6299b9bc6
Yuto Ueki <ueki.yuto.bz@gmail.com>
2023-06-23 13:14:50 +0000

    モジュールに分割したバージョン作成

---
+++ handson-v2/acm/main.tf
@@ -0,0 +1,52 @@
+variable "name" {
+  type = string
+}
+
+variable "domain" {
+  type = string
+}
+
+data "aws_route53_zone" "this" {
+  name         = "${var.domain}"
+  private_zone = false
+}
+
+resource "aws_acm_certificate" "this" {
+  domain_name = "${var.domain}"
+
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "this" {
+  depends_on = [aws_acm_certificate.this]
+
+  for_each = {
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = data.aws_route53_zone.this.id
+
+  ttl = 60
+
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "this" {
+  certificate_arn = "${aws_acm_certificate.this.arn}"
+
+  validation_record_fqdns = [for record in aws_route53_record.this : record.fqdn]
+}
+
+output "acm_id" {
+  value = "${aws_acm_certificate.this.id}"
+}
---
+++ handson-v2/main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+
+variable "name" {
+  type    = string
+  default = "myapp"
+}
+
+variable "azs" {
+  default = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+}
+
+variable "domain" {
+  type = string
+
+  default = "waito-expt.com"
+}
+
+module "network" {
+  source = "./network"
+
+  name = var.name
+
+  azs = var.azs
+}
+
+module "acm" {
+  source = "./acm"
+
+  name = var.name
+
+  domain = var.domain
+}
+
+module "elb" {
+  source = "./elb"
+
+  name = var.name
+
+  vpc_id            = module.network.vpc_id
+  public_subnet_ids = module.network.public_subnet_ids
+  domain            = var.domain
+  acm_id            = module.acm.acm_id
+}
+
+module "ecs_cluster" {
+  source = "./ecs_cluster"
+
+  name = var.name
+}
+
+module "nginx" {
+  source = "./nginx"
+
+  name = var.name
+
+  cluster_name       = module.ecs_cluster.cluster_name
+  vpc_id             = module.network.vpc_id
+  subnet_ids         = module.network.private_subnet_ids
+  https_listener_arn = module.elb.https_listener_arn
+}
