19085e6f01f36a67ebd8f8f78331c40a840b48a9
Yuto Ueki <ueki.yuto.bz@gmail.com>
2023-07-03 04:01:59 +0000

    health

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -103,7 +103,14 @@ resource "aws_lb_target_group" "default" {
   protocol    = "HTTP"
 
   health_check {
-    port = 80
+    path                = "/"
+    protocol            = "HTTP"
+    port                = 80
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+    matcher             = 200
   }
 }
 
