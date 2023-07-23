2e108bce493fe2d54156b645c3cbcccffd330eda
Yuto Ueki <ueki.yuto.bz@gmail.com>
2023-07-23 18:49:17 +0000

    コメント追加

--- modules/ssm/main.tf
+++ modules/ssm/main.tf
@@ -3,6 +3,9 @@ locals {
   base_app = "/${var.env}/${var.service}/app"
 }
 
+# TODO: デフォルトのKMSではなく、CMKを使って暗号化する
+# https://docs.aws.amazon.com/ja_jp/systems-manager/latest/userguide/sysman-paramstore-access.html#ps-kms-permissions
+
 resource "aws_ssm_parameter" "db_password" {
   name  = "${local.base_db}/password"
   type  = "SecureString"
